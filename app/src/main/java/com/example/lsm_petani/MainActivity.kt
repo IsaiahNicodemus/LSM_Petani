c63bed791a03d333422a35b0a917e90099883bfe
AZetZet <jamesalejandrox07@gmail.com>
2024-11-30 14:22:11 +0700

    Initial Commit

--- app/src/main/java/com/example/lsm_petani/MainActivity.kt
+++ app/src/main/java/com/example/lsm_petani/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.example.lsm_petani
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import android.os.Bundle
+import android.view.View
+import com.example.lsm_petani.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-//            KON
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.mainUpload.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this@MainActivity, UploadActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+        binding.mainUpdate.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this@MainActivity, UpdateActivity::class.java)
+            startActivity(intent)
+        })
+        binding.mainDelete.setOnClickListener(View.OnClickListener{
+            val intent = Intent(this@MainActivity, DeleteActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
